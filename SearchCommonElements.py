f49aabc2b1f0d98af3a77be0ca1c2fafeeb88992
Chander <101085487+keshav12-deloitte@users.noreply.github.com>
2022-05-18 12:28:47 +0530

    added SearchCommon Elements Class

---
+++ SearchCommonElements.py
@@ -0,0 +1,23 @@
+from PairsPossible import PairsPossible
+
+
+class SearchCommonElements(PairsPossible):
+
+    def commonElements(self):
+        self.string3=input("enter the string")
+        common=[]
+        for i in range(0,len(self.string1)):
+            for j in range(0,len(self.string2)):
+                for k in range(0,len(self.string3)):
+                    if(self.string1[i] == self.string2[j] and self.string1[i] == self.string3[k]):
+                        common.append(self.string1[i])
+
+        print(common)
+
+
+
+obj3=SearchCommonElements()
+obj3.strlen()
+obj3.strToChar()
+obj3.possible()
+obj3.commonElements()
